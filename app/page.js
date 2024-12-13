7133014feede609cccdb52d3941b2aad97e5d0cd
Nikhil Sharma <nikhil@amahahealth.com>
2024-12-13 19:24:03 +0530

    feat: added sf pro font

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Apple watch studio</div>;
+  return (
+    <div>
+      <h1 style={{ fontFamily: "SF Pro Text", fontWeight: 700 }}>
+        Hello World!
+      </h1>
+      <p style={{ fontFamily: "SF Pro Text", fontWeight: 400 }}>
+        This is a sample text.
+      </p>
+    </div>
+  );
 }
